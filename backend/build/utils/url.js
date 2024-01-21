3ac3c6465b2e55d7dbb6babd1849d52fdede208f
Lasse Pirinen <lassemon.pirinen@gmail.com>
2024-01-21 23:33:51 +0200

    Fix gitignore

--- backend/build/utils/url.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.constructUrl = void 0;
-const constructUrl = (paths) => {
-    return `/${paths.filter((part) => !!part).join("/")}`;
-};
-exports.constructUrl = constructUrl;
